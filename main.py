2d91f17f258e0e860fe0785e8e938791197394a5
ietzel <67884277+ietzel@users.noreply.github.com>
2025-07-08 19:20:38 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+  print("IDEA strategy")
+  print("Investigate: exploring the problem & gathering information & understanding market landscape.") 
+  print("Define: clearly articulate problem & desired outcome, ensuring a focused direction for idea generation.") 
+  print("Evaluate: assess the feasibility & potential impact & alignment of ideas with aspirations.") 
+  print("Adjust: refine & adapt ideas based on evaluation, making necessary modifications to improve their viability & effectiveness.") 
