f72a6a560633b7b652c2bf95825d1a2f5310293a
ietzel <67884277+ietzel@users.noreply.github.com>
2025-07-08 19:26:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ def main():
   print("Define: clearly articulate problem & desired outcome, ensuring a focused direction for idea generation.") 
   print("Evaluate: assess the feasibility & potential impact & alignment of ideas with aspirations.") 
   print("Adjust: refine & adapt ideas based on evaluation, making necessary modifications to improve their viability & effectiveness.") 
+  print("By following this cyclical and iterative process in tandem with applied science, the IDEA strategy effectively addresses numerous achievabilities.")
